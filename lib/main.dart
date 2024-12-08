5f1257c6db30cae257d76c5b71a98089a5eae0f3
Ahmed <ahmed.nabil1@nomadz.dev>
2024-12-08 20:17:23 +0200

    Add utils classes(Assets - Colors). Add splash image as initial route.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:islami_c13_sun/ui/screens/home/home.dart';
+import 'package:islami_c13_sun/ui/screens/splash/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +12,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      routes: {
+        Home.routeName: (_) => const Home(),
+        Splash.routeName: (_) => const Splash()
+      },
+      initialRoute: Splash.routeName,
+    );
   }
 }
---
+++ lib/ui/utils/asset_manager.dart
@@ -0,0 +1,15 @@
+abstract final class AssetsManager {
+  static const icAhadeth = "assets/images/ic_ahadeth.svg";
+  static const icRadio = "assets/images/ic_radio.svg";
+  static const icQuran = "assets/images/ic_quran.svg";
+  static const icSebha = "assets/images/ic_sebha.svg";
+  static const islamicNumbersBg = "assets/images/islamic_numbers_bg.svg";
+  static const islamiLogo = "assets/images/logo.svg";
+  static const recentSuraImage = "assets/images/recent_sura_image.svg";
+  static const titleLeftBg = "assets/images/title_bg_left.svg";
+  static const titleRightBg = "assets/images/title_bg_right.svg";
+
+  ///PNG
+  static const mainBackground = "assets/images/main_background.png";
+  static const splashImage = "assets/images/splash_image.png";
+}
